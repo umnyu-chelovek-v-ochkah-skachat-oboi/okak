83840844a839819b8716e5acc1ed8a9cd30bb3b6
umnyu-chelovek-v-ochkah-skachat-oboi <ya_vasya2019@mail.ru>
2025-11-21 16:53:22 +0300

    Delete PythonProject/my_site directory

--- PythonProject/my_site/main/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Table
-
-admin.site.register(Table)
-# Register your models here.
--- PythonProject/my_site/main/urls.py
+++
@@ -1,7 +0,0 @@
-from . import views
-from django.urls import path
-
-urlpatterns = [
-    path('', views.text),
-    path('superkrutoyurl/', views.text2)
-]
